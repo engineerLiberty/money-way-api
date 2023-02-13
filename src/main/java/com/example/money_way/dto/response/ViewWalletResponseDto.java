fc4f126b5a33532312d99d09596cb1eaf753af6a
phoenixsylvia <sylviananev@gmail.com>
2023-02-13 22:06:44 +0100

    View Balance

---
+++ src/main/java/com/example/money_way/dto/response/ViewWalletResponseDto.java
@@ -0,0 +1,17 @@
+package com.example.money_way.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ViewWalletResponseDto {
+
+    private BigDecimal balance;
+}
---
+++ src/main/java/com/example/money_way/services/impl/WalletServiceImpl.java
@@ -0,0 +1,40 @@
+package com.example.money_way.services.impl;
+
+import com.example.money_way.dto.response.ApiResponse;
+import com.example.money_way.dto.response.ViewWalletResponseDto;
+import com.example.money_way.exception.ResourceNotFoundException;
+import com.example.money_way.model.User;
+import com.example.money_way.model.Wallet;
+import com.example.money_way.repository.WalletRepository;
+import com.example.money_way.services.WalletService;
+import com.example.money_way.utils.AppUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class WalletServiceImpl implements WalletService {
+    private final WalletRepository walletRepository;
+    @Override
+    public ApiResponse viewBalance() {
+
+        ViewWalletResponseDto viewWalletResponseDto;
+        AppUtil appUtil1 = AppUtil.getInstance();
+
+        User user = appUtil1.getLoggedInUser();
+
+        Wallet wallet = walletRepository.findByUserId(user.getId())
+                .orElseThrow(()-> new ResourceNotFoundException("Wallet Not Found"));
+
+        viewWalletResponseDto = ViewWalletResponseDto.builder()
+                .balance(wallet.getBalance())
+                .build();
+
+        return ApiResponse.builder()
+                .status("SUCCESS")
+                .data(viewWalletResponseDto)
+                .build();
+    }
+
+}
+
