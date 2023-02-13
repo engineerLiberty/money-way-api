1726c92a05a28df935e2c5365410863c66fcbea6
Akuchi Ikenna <akuchiikenna16@gmail.com>
2023-02-13 16:14:45 +0100

    Create Wallet Implementation Updated

--- src/main/java/com/example/money_way/dto/request/CreateWalletRequest.java
+++ src/main/java/com/example/money_way/dto/request/CreateWalletRequest.java
@@ -1,5 +1,6 @@
 package com.example.money_way.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -8,11 +9,15 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 @Builder
 public class CreateWalletRequest {
+
     private String account_name;
+
     private String email;
+
     private String mobilenumber;
+
     private String country;
 }
-
--- src/main/java/com/example/money_way/dto/response/CreateWalletResponse.java
+++ src/main/java/com/example/money_way/dto/response/CreateWalletResponse.java
@@ -1,5 +1,6 @@
 package com.example.money_way.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -8,6 +9,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 @Builder
 public class CreateWalletResponse {
     private Long id;
