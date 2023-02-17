e9dbbcd7c9f501a43379a7bf549626186f32b47c
StanGozie <chigozie.enyoghasi@decagon.dev>
2023-02-17 11:25:02 +0100

    Resolved merge conflicts

--- src/main/java/com/example/money_way/controller/UsersController.java
+++ src/main/java/com/example/money_way/controller/UsersController.java
@@ -1,15 +1,17 @@
 package com.example.money_way.controller;
-        import com.example.money_way.dto.request.LoginRequestDto;
-        import com.example.money_way.dto.request.SignUpDto;
-        import com.example.money_way.dto.response.ApiResponse;
-        import com.example.money_way.exception.ValidationException;
-        import com.example.money_way.service.UserService;
-        import lombok.RequiredArgsConstructor;
-        import org.springframework.http.ResponseEntity;
-        import org.springframework.web.bind.annotation.PostMapping;
-        import org.springframework.web.bind.annotation.RequestBody;
-        import org.springframework.web.bind.annotation.RequestMapping;
-        import org.springframework.web.bind.annotation.RestController;
+import com.example.money_way.dto.request.LoginRequestDto;
+import com.example.money_way.dto.request.SignUpDto;
+import com.example.money_way.dto.response.ApiResponse;
+import com.example.money_way.exception.ValidationException;
+import com.example.money_way.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import javax.validation.Valid;
+
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/v1/auth")
@@ -20,7 +22,7 @@ public class UsersController {
         return userService.login(loginRequestdto);
     }
     @PostMapping("/sign-up")
-    public ResponseEntity<ApiResponse> signUp(@Valid @RequestBody SignUpDto signUpDto) throws ValidationException {
+    public ResponseEntity<ApiResponse> signUp(@Valid @RequestBody SignUpDto signUpDto) {
         return userService.signUp(signUpDto);
     }
 }
