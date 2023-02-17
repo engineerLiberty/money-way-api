4d1c0938419ff2c6a7c86017e1317bf616b6022e
IMOBI LIBERTY <107262741+engineerLiberty@users.noreply.github.com>
2023-02-17 11:51:04 +0100

    verify link implementation update

--- src/main/java/com/example/money_way/controller/UserController.java
+++ src/main/java/com/example/money_way/controller/UserController.java
@@ -1,22 +1,22 @@
 package com.example.money_way.controller;
+        import com.example.money_way.dto.request.LoginRequestDto;
+        import com.example.money_way.dto.request.VerifyTokenDto;
+        import com.example.money_way.dto.response.ApiResponse;
+        import com.example.money_way.service.UserService;
+        import lombok.RequiredArgsConstructor;
+        import org.springframework.http.HttpStatus;
+        import org.springframework.http.ResponseEntity;
+        import org.springframework.web.bind.annotation.*;
 
-import com.example.money_way.dto.request.VerifyTokenDto;
-import com.example.money_way.dto.response.ApiResponse;
-import com.example.money_way.service.UserService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RequestMapping("/api/v1")
-@RestController
 @RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/v1/auth")
 public class UserController {
-    private  final UserService userService;
-
+    private final UserService userService;
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody LoginRequestDto loginRequestdto) {
+        return userService.login(loginRequestdto);
+    }
     @PutMapping("/verify-link")
     ResponseEntity<ApiResponse>verifyLink(@RequestBody VerifyTokenDto verifyTokenDto){
         ApiResponse response = userService.verifyLink(verifyTokenDto);
